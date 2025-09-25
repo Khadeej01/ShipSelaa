a65f591379c14ba2b2eeea0db722ca0ee346f9f0
khadijaDev <khadijaoukhlift@gmail.com>
2025-09-25 12:01:56 +0200

    Architecture initiale du projet

---
+++ src/main/java/com/shipselaa/ShipSelaaApplication.java
@@ -0,0 +1,13 @@
+package com.shipselaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipSelaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipSelaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shipselaa/ShipSelaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipselaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipSelaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
